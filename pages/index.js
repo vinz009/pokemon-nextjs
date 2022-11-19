062a84665779e839dd9a46bbdc40fe757021f24d
Vincent <vincentamoguis@gmail.com>
2022-11-19 21:03:27 +0800

    chore: Added 10 item pagination

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,46 @@
 import axios from 'axios';
+import { useState } from 'react';
 
 export default function HomePage({ data }) {
-	console.log(data);
+	
+	const [pokemon, setPokemon] = useState(data);
 
-const lists = data.results.map((pokemon) => (
+const lists = pokemon.results.map((pokemon) => (
 	<div>
 	Name: {pokemon.name}		
 	</div>
 ));
 
+function handleNext () {
+
+	axios.get(pokemon.next)
+	.then(function (response) {
+		console.log(response.data);
+		setPokemon(response.data);
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+}
+
+
+function handlePrevious () {
+
+	axios.get(pokemon.previous)
+	.then(function (response) {
+		console.log(response.data);
+		setPokemon(response.data);
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+}
+
   return (
 	  <Container>
 		  <Header />
 		  <Main  />
-		  <SideBar lists={lists} />
+		  <SideBar lists={lists} handleNext={handleNext} handlePrevious={handlePrevious} />
 	  </Container>
   );
 }
@@ -47,11 +74,21 @@ function Main ( ) {
 	);
 }
 
-function SideBar ({lists}) {
+function SideBar ({lists, handleNext, handlePrevious}) {
 	return (
+		<>
 		<div>
 		{lists}
 		</div>
+		<div>
+		<button onClick={handleNext} >
+		Next
+		</button>
+		<button onClick={handlePrevious} >
+		Previous
+		</button>
+		</div>
+		</>
 	);
 }
 
